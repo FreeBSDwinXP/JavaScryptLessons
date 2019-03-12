333ad4a7771f259077d93452d55e2c2266ceab6b
FreeBSDwinXp <freebsdwinxp@gmail.com>
2019-03-13 00:26:20 +0200

    jQuery

--- 17_JQuery/js/script.js
+++ 17_JQuery/js/script.js
@@ -1,16 +1,14 @@
 $(document).ready(function(){
-
     function modalOn() {
         $('.overlay').fadeIn('slow');
         $('.modal').slideDown();
     }
 
-    function modalOff() {
+    $('.close').on('click', function() {
         $('.overlay').fadeOut('slow');
         $('.modal').slideUp();
-    }
+    });
 
-    $('.close').on('click', modalOff);
     $('.main_btna').on('click', modalOn);
     $('.main_btn').on('click', modalOn);
     $('li:contains("расписания туров")').on('click', modalOn);
